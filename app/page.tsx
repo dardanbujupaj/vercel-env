e8138b2d4d75513ebf50a8921493874959f91bf7
Dardan Bujupaj <dardan@ax.tech>
2024-02-22 11:30:27 +0100

    change header

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div className="dark:prose-invert prose prose-stone mx-auto">
-        <h2>Environment</h2>
+        <h1>Environment</h1>
         <pre>{JSON.stringify(process.env, null, 2)}</pre>
       </div>
     </main>
